cdf1a9b97b5da452b658a2a766b2189f2c026a53
romaing <rocambille@gmail.com>
2023-03-31 14:27:37 +0200

    added props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,23 @@ import PokemonCard from "./components/PokemonCard";
 
 import "./App.css";
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function App() {
+  const pokemon = pokemonList[0];
+
   return (
     <div className="App">
-      <PokemonCard />
+      <PokemonCard pokemon={pokemon} />
     </div>
   );
 }
