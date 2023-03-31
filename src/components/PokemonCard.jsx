5ed0925c721a782b956b5e7be396f7e7f5116822
romaing <rocambille@gmail.com>
2023-03-31 14:19:12 +0200

    added dynamic data

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,25 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+
   return (
     <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-      <figcaption>bulbasaur</figcaption>
+      {pokemon.imgSrc == null ? (
+        <p>???</p>
+      ) : (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
